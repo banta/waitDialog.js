8e81707d4e9c029e6f5232bad670d953f35cf8a0
Bernard Banta <banta.bernard@gmail.com>
2014-05-07 11:55:52 +0300

    Added wait-dialog.js file

---
+++ wait-dialog.js
@@ -0,0 +1,12 @@
+var waitDialog;
+waitDialog = waitDialog || (function () {
+    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1 id="wait_dialog_text"></h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+    return {
+        showPleaseWait: function() {
+            pleaseWaitDiv.modal();
+        },
+        hidePleaseWait: function () {
+            pleaseWaitDiv.modal('hide');
+        }
+    };
+})();
